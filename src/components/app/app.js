3182379a07ad219eb0f009528bff482a2f2ad8f3
Yura <ybeliaev@gmail.com>
2019-03-09 19:08:40 +0200

    app element

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import List from "../list";
+import "./app.css";
+
+export default class App extends Component {
+  maxId = 10;
+
+  state = {
+    someData: [
+      this.createItem("Drink Coffee", true, true),
+      this.createItem("Create React App"),
+      this.createItem("Study Bible", false, true),
+      this.createItem("Read book of Hemingway")
+    ]
+  };
+  createItem(label, done = false, important = false) {
+    return {
+      label,
+      done,
+      important,
+      id: this.maxId++
+    };
+  }
+  render() {
+    return (
+      <div className="app">
+        <List someProps={this.state.someData} />
+      </div>
+    );
+  }
+}
