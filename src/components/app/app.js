72bc2ed6fb542d99e1a6354b8fc06bb85a1785f6
Yura <ybeliaev@gmail.com>
2019-06-13 14:33:10 +0300

    add h1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -26,6 +26,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
+        <h1>Компоненты с состоянием</h1>
         <List someProps={this.state.someData} />
       </div>
     );
